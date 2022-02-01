ba07551eaf9fc4adac7af1f4975988b9e4bb524b
maikereis <maikerdralcantara@gmail.com>
2022-02-01 18:06:22 -0300

    Remove tests

--- tests/test_download_dataset.py
+++ tests/test_download_dataset.py
@@ -24,15 +24,3 @@ def test_nothing():
     """This is just a sample of test."""
     var = 1
     assert var == 1
-
-
-def test_nothing2():
-    """This is just a sample of test."""
-    var2 = 1
-    assert var2 == 1
-
-
-def test_nothing3():
-    """This is just a sample of test."""
-    var3 = 1
-    assert var3 == 1
